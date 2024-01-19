d19027637f28702d6cd3bf248f13db2e661c7f28
vresende <vini_handball@hotmail.com>
2024-01-19 15:25:55 -0300

    ajustes-tema-hybrix-2: Instalação do sweetalert e arquivo de configuração do site

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name' => 'Hybrix',
+    'description' => 'Painel de administração',
+];
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,10 @@
 
 <head>
     <meta charset="utf-8" />
-    <title> @yield('title') | Hybrix - Laravel 10 Admin & Dashboard Template </title>
+    <title> @yield('title') | {{ config('site.name') }} </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="{{ config('site.description') }}" name="description" />
+    <meta content="Vinicius Resende" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico') }}">
     @include('layouts.head-css')
@@ -23,6 +23,8 @@
         <!-- ============================================================== -->
         <div class="main-content">
             <div class="page-content">
+
+
                 <div class="container-fluid">
                     @yield('content')
                 </div>
@@ -34,7 +36,7 @@
         <!-- end main content-->
     </div>
     <!-- END layout-wrapper -->
-
+    @include('sweetalert::alert')
     @include('layouts.customizer')
     <!-- JAVASCRIPT -->
     @include('layouts.vendor-scripts')
